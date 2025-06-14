cbcb9b201d10a358e7513306c3c7d1dcb09ca659
Georgy Lepsaya <georgelepsaya@gmail.com>
2025-06-15 00:07:18 +0300

    refactor: removed taxonomy loader

--- app/utils/parse_taxonomy_dmp.py
+++ app/utils/parse_taxonomy_dmp.py
@@ -1,29 +1,34 @@
 from collections import defaultdict
 from typing import Any
+from tqdm import tqdm
 
 
 def parse_names_dmp(file_path: str):
     names: dict[str, dict[str, Any]] = defaultdict(lambda: {"synonyms": []})
     
     with open(file_path, "r") as f:
-        for line in f:
-            parts = [p.strip() for p in line.split("|")]
-            tax_id, name_txt, _, name_class = parts[:4]
-            if name_class == "scientific name":
-                names[tax_id]["scientific_name"] = name_txt
-            elif name_class == "synonym":
-                names[tax_id]["synonyms"].append(name_txt)
+        lines = f.readlines()
+    
+    for line in tqdm(lines, desc="Parsing names.dmp"):
+        parts = [p.strip() for p in line.split("|")]
+        tax_id, name_txt, _, name_class = parts[:4]
+        if name_class == "scientific name":
+            names[tax_id]["scientific_name"] = name_txt
+        elif name_class == "synonym":
+           names[tax_id]["synonyms"].append(name_txt)
 
     return names
 
 
 def parse_nodes_dmp(file_path: str):
     nodes: dict[str, dict[str, str]] = {}
-
+    
     with open(file_path, "r") as f:
-        for line in f:
-            parts = [p.strip() for p in line.split("|")]
-            tax_id, parent_tax_id, rank = parts[0], parts[1], parts[2]
-            nodes[tax_id] = {"parent": parent_tax_id, "rank": rank}
+        lines = f.readlines()
+
+    for line in tqdm(lines, desc="Parsing nodes.dmp"):
+        parts = [p.strip() for p in line.split("|")]
+        tax_id, parent_tax_id, rank = parts[0], parts[1], parts[2]
+        nodes[tax_id] = {"parent": parent_tax_id, "rank": rank}
     
     return nodes
